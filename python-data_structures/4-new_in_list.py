f9d4445156adc92101372579818a59603c41c055
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-10-25 10:40:56 -0700

    4-new_in_list.py

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a list at a specific position."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
