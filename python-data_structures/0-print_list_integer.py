f6e95b856b88c8c832b4672ed7a300b79005f273
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-10-25 09:52:09 -0700

    0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,7 +3,7 @@
 def print_list_integer(my_list=[]):
     """Prints all integers of a list, one integer per line."""
     for integer in my_list:
-        if type(integer) is not int:  # Check for non-integer types
+        if not isinstance(integer, int):  # Check if the element is an integer
             raise TypeError("list must contain only integers")
         print("{}".format(integer))
 
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     print_list_integer([1, 2, 3])  # Expected output: 1\n2\n3
     print_list_integer([1])         # Expected output: 1
     print_list_integer([])          # Expected output: (nothing printed)
+    
     try:
         print_list_integer([1, 2, "H", 9])  # Should raise an exception
     except TypeError as e:
