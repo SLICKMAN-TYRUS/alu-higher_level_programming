6b679429922e8fcec988a63e540c577efdbd7e84
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-11-09 02:06:41 -0800

    0. Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defining a Rectangle class.'''
+
+
+class Rectangle:
+    """
+    This is an empty Rectangle class that defines a rectangle.
+
+    Attributes:
+        None
+    """
+
+    pass
