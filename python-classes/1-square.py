15300bfb11bee0eaab1e29e27792f3b8b62378eb
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-11-09 01:01:41 -0800

    1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with a private attribute __size.
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with the given size.
+        """
+        self.__size = size  # Private attribute
