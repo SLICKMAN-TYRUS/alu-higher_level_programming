055f8bc8dddf097ccc905b035270ad5e19223863
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-11-15 03:31:15 -0800

    8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,5 @@
--- Write a script that creates the database hbtn_0d_usa and the table cities on your MySQL server.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities` (
-    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    state_id INT NOT NULL, FOREIGN KEY(`state_id`) REFERENCES states(`id`),
-    name VARCHAR(256) NOT NULL
-);
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id = (
+	SELECT id FROM states WHERE name = "California"
+)
+ORDER BY cities.id ASC;
