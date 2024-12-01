1058b1705628a5074e034cfedb71033646f17020
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-12-01 19:54:33 +0300

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbnt.io/status
+"""Fetches url
+using the request moduel
 """
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
